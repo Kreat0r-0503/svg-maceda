6f3caccf42cd91528600dea4036eda67a2b416b7
Ec da KreatOr <64685270+Kreat0r-0503@users.noreply.github.com>
2020-10-22 23:26:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="em">
+<meta charset="UTF-8">
+	<meta name="viewport" content ="width-device-width, initial-scale=1.0">
+	<meta http-eqiv="X-UA-compatible" content="ie-edge">
+	<title> SVG </title>
+	<link rel="stylesheet" href="styel.css"
+  <svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+    <!-- Created with SVG-edit - https://github.com/SVG-Edit/svgedit-->
+    <g class="layer">
+     <title>Layer 1</title>
+     <rect fill="#56aaff" height="223.99999" id="svg_78" stroke="#56aaff" stroke-width="5" width="634.99999" x="3.5" y="253"/>
+     <rect fill="#56aaff" height="209" id="svg_76" stroke="#56aaff" stroke-width="5" width="634.99999" x="4.00001" y="2"/>
+     <line fill="none" id="svg_9" stroke="#cccccc" stroke-width="5" x1="21" x2="21" y1="0" y2="156"/>
+     <line fill="none" id="svg_11" stroke="#cccccc" stroke-width="5" x1="55" x2="55" y1="-2" y2="94"/>
+     <line fill="none" id="svg_12" stroke="#cccccc" stroke-width="5" x1="83" x2="83" y1="1.5" y2="157.5"/>
+     <line fill="none" id="svg_13" stroke="#cccccc" stroke-width="5" x1="112" x2="112" y1="-0.5" y2="103.50001"/>
+     <line fill="none" id="svg_15" stroke="#cccccc" stroke-width="5" x1="508" x2="508" y1="1.5" y2="97.50001"/>
+     <line fill="none" id="svg_16" stroke="#cccccc" stroke-width="5" x1="549" x2="549" y1="0.5" y2="141.5"/>
+     <line fill="none" id="svg_17" stroke="#cccccc" stroke-width="5" x1="618" x2="618" y1="0.5" y2="156.5"/>
+     <line fill="none" id="svg_18" stroke="#cccccc" stroke-width="5" x1="587" x2="587" y1="0.5" y2="100.5"/>
+     <ellipse cx="20" cy="169.5" fill="#ff5656" id="svg_19" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <ellipse cx="55" cy="111.5" fill="#ff5656" id="svg_22" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <ellipse cx="83" cy="173.5" fill="#ff5656" id="svg_23" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <ellipse cx="112" cy="119.5" fill="#ff5656" id="svg_24" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <ellipse cx="508" cy="112.5" fill="#ff5656" id="svg_25" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <ellipse cx="550" cy="158.5" fill="#ff5656" id="svg_26" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <ellipse cx="589" cy="115.5" fill="#ff5656" id="svg_27" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <ellipse cx="619" cy="175.5" fill="#ff5656" id="svg_28" rx="15" ry="16.5" stroke="#ff5656" stroke-width="5"/>
+     <rect fill="#00ffff" height="35" id="svg_1" stroke="#cccccc" stroke-width="5" width="316.00001" x="4" y="213"/>
+     <rect fill="#00ffff" height="35" id="svg_6" stroke="#cccccc" stroke-width="5" width="313.99999" x="324.5" y="213"/>
+     <polyline fill="none" id="svg_2" points="114.00004023129077,3.999999761581421 192.20005063715507,92 270.400053354635,3.999999761581421 348.60002531857754,92 426.7999665289826,3.999999761581421 505,92 " stroke="#cccccc" stroke-width="5"/>
+     <ellipse cx="368" cy="295" fill="none" id="svg_34" rx="0" ry="1" stroke="#000000" stroke-width="5"/>
+     <ellipse cx="306" cy="311" fill="none" id="svg_40" rx="1" ry="0" stroke="#000000" stroke-width="5"/>
+     <polygon fill="none" id="svg_47"/>
+     <polygon fill="none" id="svg_48"/>
+     <polygon fill="none" id="svg_49" points="195.455690318826,274 184.08508796268853,289.65029150785455 165.68706687789847,283.67241208569794 165.68706687789847,264.32758791430206 184.08508796268853,258.34970849214545 195.455690318826,274 " stroke-width="5"/>
+     <polygon fill="none" id="svg_50"/>
+     <polygon fill="none" id="svg_51" points="145.56710053890137,272 145.1752437040397,272.5393446629166 144.5412060265096,272.3333333333333 144.5412060265096,271.6666666666667 145.1752437040397,271.4606553370834 145.56710053890137,272 " stroke-width="5"/>
+     <polygon fill="none" id="svg_52"/>
+     <polygon fill="none" id="svg_53"/>
+     <polygon fill="none" id="svg_54"/>
+     <polygon fill="none" id="svg_55" points="203.65737915039062,353.49999952370854 175.5942030697115,322.1847229003906 130.18702714311803,334.14615657320974 130.18702714311803,372.85384247420734 175.5942030697115,384.81527614702645 203.65737915039062,353.49999952370854 " stroke-width="5"/>
+     <polygon fill="none" id="svg_56"/>
+     <polygon fill="none" id="svg_57" points="184.89364561710158,333 177.36632162638256,343.3604726503546 165.18685556506665,339.40312423743285 165.18685556506665,326.59687576256715 177.36632162638256,322.6395273496454 184.89364561710158,333 " stroke-width="5"/>
+     <polygon fill="none" id="svg_58" points="154,294 154,294 154,294 154,294 154,294 154,294 " stroke-width="5"/>
+     <polygon fill="none" id="svg_59"/>
+     <polygon fill="none" id="svg_60" points="247.84852600097656,326 234.13352966308594,344.8770751953125 211.9422149658203,337.6666564941406 211.9422149658203,314.3333435058594 234.13352966308594,307.1229248046875 247.84852600097656,326 " stroke-width="5"/>
+     <polygon fill="none" id="svg_61" points="203.78330993652344,273 199.78713989257812,278.500244140625 193.32119750976562,276.39935302734375 193.32119750976562,269.60064697265625 199.78713989257812,267.499755859375 203.78330993652344,273 " stroke-width="5"/>
+     <polygon fill="none" id="svg_62" points="187.44953790894112,361 185.06596583660343,364.2807055065056 181.209265208926,363.0275875100994 181.209265208926,358.9724124899006 185.06596583660343,357.7192944934944 187.44953790894112,361 " stroke-width="5"/>
+     <polygon fill="none" id="svg_63" points="148.51563719192183,403 135.72164655398467,420.60941740473413 115.0205348500544,413.8832184782097 115.0205348500544,392.1167815217903 135.72164655398467,385.39058259526587 148.51563719192183,403 " stroke-width="5"/>
+     <polygon fill="none" id="svg_64" points="238.56710053890137,422 238.1752437040397,422.5393446629166 237.5412060265096,422.3333333333333 237.5412060265096,421.6666666666667 238.1752437040397,421.4606553370834 238.56710053890137,422 " stroke-width="5"/>
+     <polygon fill="none" id="svg_65" points="228,421 228,421 228,421 228,421 228,421 228,421 " stroke="#000000" stroke-width="5"/>
+     <polygon fill="none" id="svg_69"/>
+     <polygon fill="none" id="svg_70" points="286.91823539521096,335 275.22802225195596,351.0901980168307 256.3128600504386,344.94428926011756 256.3128600504386,325.05571073988244 275.22802225195596,318.9098019831693 286.91823539521096,335 " stroke-width="5"/>
+     <polygon fill="none" id="svg_71"/>
+     <polygon fill="none" id="svg_72"/>
+     <rect fill="none" height="0" id="svg_77" stroke="#000000" stroke-width="5" width="4" x="425" y="172"/>
+     <ellipse cx="323.50002" cy="351.49999" fill="#ff5656" id="svg_81" rx="25.14677" ry="52.25904" stroke="#cccccc" stroke-width="5" transform="rotate(0.364907 323.5 351.5)"/>
+     <polygon fill="#56aaff" id="svg_3" points="180.71340125535482,316 175.3835720723377,323.33588052670444 166.7597272999849,320.5338235029118 166.7597272999849,311.4661764970882 175.3835720723377,308.66411947329556 180.71340125535482,316 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#56aaff" id="svg_4" points="138.06006298307122,332 126.96283239250103,347.2740275521605 109.00713611596336,341.4398681723375 109.00713611596336,322.5601318276625 126.96283239250101,316.7259724478395 138.06006298307122,332 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_5" points="260.93328857421875,344 253.12527465820312,353.0555725097656 240.4916534423828,349.5966491699219 240.4916534423828,338.4033508300781 253.12527465820312,334.9444274902344 260.93328857421875,344 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_8"/>
+     <polygon fill="#ff5656" id="svg_10"/>
+     <polygon fill="#ff5656" id="svg_14" points="205,304 205,304 205,304 205,304 205,304 205,304 " stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_20"/>
+     <polygon fill="#ff5656" id="svg_21"/>
+     <polygon fill="#ff5656" id="svg_29"/>
+     <polygon fill="#ff5656" id="svg_30"/>
+     <polygon fill="#ff5656" id="svg_31"/>
+     <polygon fill="#ff5656" id="svg_33"/>
+     <polygon fill="#ff5656" id="svg_35" points="276.2208251953125,298.5 268.4128112792969,307.5555725097656 255.7791748046875,304.0966491699219 255.7791748046875,292.9033508300781 268.4128112792969,289.4444274902344 276.2208251953125,298.5 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_36" points="335.2208251953125,276.5 327.4128112792969,285.5555725097656 314.7791748046875,282.0966491699219 314.7791748046875,270.9033508300781 327.4128112792969,267.4444274902344 335.2208251953125,276.5 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_37" points="392.2208251953125,301.5 384.4128112792969,310.5555725097656 371.7791748046875,307.0966491699219 371.7791748046875,295.9033508300781 384.4128112792969,292.4444274902344 392.2208251953125,301.5 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_38" points="399.2208251953125,351.5 391.4128112792969,360.5555725097656 378.7791748046875,357.0966491699219 378.7791748046875,345.9033508300781 391.4128112792969,342.4444274902344 399.2208251953125,351.5 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_39" points="389.2208251953125,401.5 381.4128112792969,410.5555725097656 368.7791748046875,407.0966491699219 368.7791748046875,395.9033508300781 381.4128112792969,392.4444274902344 389.2208251953125,401.5 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_41" points="281.2208251953125,395.5 273.4128112792969,404.5555725097656 260.7791748046875,401.0966491699219 260.7791748046875,389.9033508300781 273.4128112792969,386.4444274902344 281.2208251953125,395.5 " stroke="#56aaff" stroke-width="5"/>
+     <polygon fill="#ff5656" id="svg_42" points="335.2208251953125,429.5 327.4128112792969,438.5555725097656 314.7791748046875,435.0966491699219 314.7791748046875,423.9033508300781 327.4128112792969,420.4444274902344 335.2208251953125,429.5 " stroke="#56aaff" stroke-width="5"/>
+    </g>
+   </svg>
+</html>
+
